322056efa90206c6a65dfa5d8375925177ddd996
Zechariah hounwanou <zechariahhounwanou@gmail.com>
2024-05-21 00:39:10 +0100

    Added the connection string and connected the server to the Atlas DB

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from "express";
 import morgan from "morgan";
 import { nanoid } from "nanoid";
+import connectDB from "./db/connectDB.js";
 
 const app = express();
 
@@ -26,49 +27,6 @@ app.get("/api/v1/jobs", (req, res) => {
   res.status(200).json({ jobs });
 }); // Get all Jobs
 
-app.post("/api/v1/jobs", (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: "Please Provide Company and Position" });
-  }
-  const id = nanoid(10);
-  const job = { id, company, position };
-  jobs.push(job);
-  res.status(201).json({ job });
-}); // Create a Job
-
-app.get("/api/v1/jobs/:id", (req, res) => {
-  const { id: jobId } = req.params;
-  const singleJob = jobs.find((job) => job.id === jobId);
-  if (!singleJob) {
-    return res.status(400).json({ msg: "Job ID does not Exist" });
-  }
-  res.status(200).json({ singleJob });
-}); // Get Single Job
-
-app.patch("/api/v1/jobs/:id", (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: "Please Provide Company and Position" });
-  }
-
-  const { id: jobId } = req.params;
-  const singleJob = jobs.find((job) => job.id === jobId);
-
-  singleJob.company = company;
-  singleJob.position = position;
-  res.status(200).json({ singleJob });
-}); // Updating Job
-
-app.delete("/api/v1/jobs/:id", (req, res) => {
-  const { id: jobId } = req.params;
-  const singleJob = jobs.find((job) => job.id === jobId);
-  const newJobs = jobs.filter((job) => job.id !== jobId);
-  jobs = newJobs;
-
-  res.status(200).json({ msg: "Job Deleted Successfully" });
-}); // Delete Job
-
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "Route Not Found" });
 }); // Triggers when req made to a URL does not exist
@@ -80,6 +38,11 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5100; // stating PORT location
 
-app.listen(PORT, () => {
-  console.log("Server is up!");
-});
+try {
+  await connectDB(process.env.MONGO_URL);
+  app.listen(PORT, () => {
+    console.log("Server is up and DB is Connected");
+  });
+} catch (error) {
+  console.log(error);
+}
